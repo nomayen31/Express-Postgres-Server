37792c8dcaf722badfb673be30145ae60fd85139
nomayen31 <nomyen.ohin@gmail.com>
2025-11-30 08:39:39 +0600

    [user created and insert on the DB]

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from "express";
 import { Pool } from "pg";
 import dotenv from "dotenv";
 import path from "path";
-dotenv.config({path: path.join(process.cwd(), ".env")});
+dotenv.config({ path: path.join(process.cwd(), ".env") });
 const app = express();
 const port = 5000;
 
@@ -15,33 +15,33 @@ const pool = new Pool({
 
 
 
-const initDB = async () =>{
+const initDB = async () => {
   await pool.query(`
-    CREATE TABLE IF NOT EXISTS users(
-      id SERIAL PRIMARY KEY,
-      name VARCHAR(100) NOT NULL,
-      email VARCHAR(100) NOT NULL,
-      age INT,
-      phone VARCHAR(15),
-      address TEXT,
-      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-      updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    )
-  `);
+        CREATE TABLE IF NOT EXISTS users(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(100) NOT NULL,
+        email VARCHAR(150) UNIQUE NOT NULL,
+        age INT,
+        phone VARCHAR(15),
+        address TEXT,
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP DEFAULT NOW()
+        )
+        `);
 
   await pool.query(`
-    CREATE TABLE IF NOT EXISTS todos(
-      id SERIAL PRIMARY KEY,
-      user_id INT REFERENCES users(id) ON DELETE CASCADE,
-      title VARCHAR(100) NOT NULL,
-      description TEXT,
-      completed BOOLEAN DEFAULT FALSE,
-      due_date DATE,
-      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-      updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    )
-  `);
-}
+            CREATE TABLE IF NOT EXISTS todos(
+            id SERIAL PRIMARY KEY,
+            user_id INT REFERENCES users(id) ON DELETE CASCADE,
+            title VARCHAR(200) NOT NULL,
+            description TEXT,
+            completed BOOLEAN DEFAULT false,
+            due_date DATE,
+            created_at TIMESTAMP DEFAULT NOW(),
+            updated_at TIMESTAMP DEFAULT NOW()
+            )
+            `);
+};
 
 initDB();
 
@@ -49,12 +49,25 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello This is next level developer!");
 });
 
-app.post('/', (req: Request, res: Response) => {
-  console.log(req.body);
-  res.status(201).json({
-    success: true,
-    message: "Data received successfully"
-  })
+app.post('/users', async (req: Request, res: Response) => {
+  const { name, email, age, phone, address } = req.body;
+  try {
+    const result = await pool.query(`
+      INSERT INTO users(name , email , age , phone , address)
+      VALUES($1 , $2 , $3 , $4 , $5) RETURNING *
+    `, [name, email, age, phone, address]);
+    console.log(result.rows[0]);
+    res.status(201).json({
+      success: true,
+      message: "User created successfully",
+      data: result.rows[0]
+    })
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message
+    })
+  }
 })
 
 app.listen(port, () => {
